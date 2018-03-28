856d96f9612e713425640984590afc64ba6120e4
Simon Accascina <simon@accascina.me>
2018-03-28 16:44:29 +0200

    Save file

--- background.js
+++ background.js
@@ -1,53 +1,17 @@
 var screenshot = {
-	content : document.createElement("canvas"),
-	data : '',
 
-	init : function() {
-		this.initEvents();
-	},
-	
-	saveScreenshot : function() {
-		var image = new Image();
-		image.onload = function() {
-			var canvas = screenshot.content;
-			canvas.width = image.width;
-			canvas.height = image.height;
-			var context = canvas.getContext("2d");
-			context.drawImage(image, 0, 0);
-
-			// save the image
-			var link = document.createElement('a');
-			link.download = "download.png";
-			link.href = screenshot.content.toDataURL();
-			// link.click();
-			chrome.downloads.download({
-   			    url: screenshot.data,
-        		filename: "maps/relative.png"
-      		});
-			console.log('sda4')
-			screenshot.data = '';
-		};
-		image.src = screenshot.data; 
-	},
-	
-	initEvents : function() {
+	init: function() {
 		chrome.browserAction.onClicked.addListener(function(tab) {
 			chrome.tabs.captureVisibleTab(null, {
 				format : "png",
 				quality : 100
 			}, function(data) {
-				screenshot.data = data;
-				
-				// send an alert message to webpage
-				chrome.tabs.query({
-					active : true,
-					currentWindow : true
-				}, function(tabs) {
-					chrome.tabs.sendMessage(tabs[0].id, {ready : "ready"}, function(response) {
-						screenshot.saveScreenshot();
-					});
-				}); 
-
+				let name = tab.url
+				name = name.substring(name.lastIndexOf("@") + 1, name.lastIndexOf("z"));
+				chrome.downloads.download({
+   				    url: data,
+   		     		filename: "maps/" + name + ".png"
+				});
 			});
 		});
 	}
