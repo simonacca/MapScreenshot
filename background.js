e5f22beb9acffc2545fe773a2a24142e3527f19a
Simon Accascina <simon@accascina.me>
2018-03-28 17:01:34 +0200

    Use regex for match

--- background.js
+++ background.js
@@ -7,10 +7,13 @@ var screenshot = {
 				quality : 100
 			}, function(data) {
 				let name = tab.url
-				name = name.substring(name.lastIndexOf("@") + 1, name.lastIndexOf("m"));
+				// name = name.substring(name.lastIndexOf("@") + 1, name.lastIndexOf("m"));
+				name = /@([^m]+)m/.exec(name);
+				console.log(name)
+
 				chrome.downloads.download({
    				    url: data,
-   		     		filename: "maps/" + name + ".png"
+   		     		filename: "maps/" + name[1] + ".png"
 				});
 			});
 		});
