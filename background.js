72f055b519452cd572a3270ed3164278934afb12
Simon Accascina <simon@accascina.me>
2018-03-28 17:11:06 +0200

    Add keyboard shortcut

--- background.js
+++ background.js
@@ -1,22 +1,34 @@
 var screenshot = {
 
-	init: function() {
-		chrome.browserAction.onClicked.addListener(function(tab) {
-			chrome.tabs.captureVisibleTab(null, {
-				format : "png",
-				quality : 100
-			}, function(data) {
-				let name = tab.url
-				// name = name.substring(name.lastIndexOf("@") + 1, name.lastIndexOf("m"));
-				name = /@([^m]+)m/.exec(name);
-				console.log(name)
+	takeScreenshot: function(){
+		chrome.tabs.getSelected(null, function(tab) {
+		chrome.tabs.captureVisibleTab(null, {
+			format : "png",
+			quality : 100
+		}, function(data) {
+			let name = tab.url
+			name = /@([^m]+)m/.exec(name);
+			console.log(name)
 
-				chrome.downloads.download({
-   				    url: data,
-   		     		filename: "maps/" + name[1] + ".png"
-				});
+			chrome.downloads.download({
+				url: data,
+				filename: "maps/" + name[1] + ".png"
 			});
 		});
+    });
+	},
+	init: function() {
+		chrome.browserAction.onClicked.addListener(function(tab) {
+			screenshot.takeScreenshot()
+		});
+		chrome.commands.onCommand.addListener(function(command) {
+			switch(command){
+				case 'take-screenshot': {
+					screenshot.takeScreenshot()
+				}
+			}
+		});
+      
 	}
 };
 
